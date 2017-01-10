b2aef5dd47751b37c6197d9571d270a61ccb06f7
edzhou2 <edward.zhou@smsmt.com>
2017-01-10 17:18:02 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
@@ -10,7 +10,7 @@ var uuid = require('node-uuid');
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
