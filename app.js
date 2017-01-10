445420205a57a1322e76a3e22280024a6ec616ae
edzhou2 <edward.zhou@smsmt.com>
2017-01-10 17:23:34 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-//var logger = require('morgan');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
@@ -10,7 +10,7 @@ var uuid = require('node-uuid');
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -20,7 +20,7 @@ app.use(session({
     genid: function(req) { return uuid.v4(); },
     secret: 'securebank'
 }));
-
+console.log("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
 app.use(require('./routes/index'));
 app.use(require('./routes/connect_auth'));
 app.use(require('./routes/mobile_connect_auth'));
